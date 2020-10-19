a7ca1994927a664f16df9701e7d5782e9ab8e797
herovisser <herovisser@MacBook-Pro-van-Hero.local>
2020-10-19 23:02:35 +0200

    c

--- pwd-game/hangman.js
+++ pwd-game/hangman.js
@@ -195,7 +195,7 @@ window.onload = function() {
         chosenCategory = categories[Math.floor(Math.random() * categories.length)];
         word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
         word = word.replace(/\s/g, "-");
-        console.log(word);
+        //  console.log(word);
         buttons();
 
         geusses = [];
